c64f066265f1a5ca0ed4bd05c16a42f0b88802a8
Jason Johnson <spligak@gmail.com>
2014-03-02 03:38:49 -0600

    start packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='microrpc',
+    version='0.1',
+    description='A micro framework for creating RPC clients and servers.',
+    author='Jason Johnson',
+    author_email='spligak@gmail.com',
+    packages=['microrpc'],
+    license='MIT',
+    zip_safe=False,
+    url='http://github.com/jasonjohnson/microrpc'
+)
+
